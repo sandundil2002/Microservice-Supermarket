00f978ccd9ccc5c1eda19f9686578650f92d8099
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2025-02-19 19:41:03 +0530

    gradle issue fixed.

--- Order-Service/src/main/java/lk/ijse/orderservice/controller/OrderController.java
+++ Order-Service/src/main/java/lk/ijse/orderservice/controller/OrderController.java
@@ -1,3 +1,9 @@
+package lk.ijse.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
